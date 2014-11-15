8eab90aab61a5e510e9b2cc23d3662b879ae1867
SAC-CS <huynh_durendal@sac.edu>
2014-11-15 09:59:36 -0800

    Update Computer.java

--- Homework/Computer.java
+++ Homework/Computer.java
@@ -3,6 +3,7 @@ package Homework;
 // class Computer generates a random number to represent
 // the computer's choice determines the winner between
 // the user and the computer
+// Demonstrate inheritance
 public class Computer extends Game
 {
 	// A private integer variable used to store the random
@@ -10,19 +11,26 @@ public class Computer extends Game
 	private int randoms = generator();
 	
 	// constructor with one parameter
+	// Demonstrate inheritance of superclass Constructor
 	public Computer(int user)
 	{
 		super(user);
 	}
 	
 	// Creating a random number to represent the computer's choice
+	//  This should be coming from the parent/supper class, right?
 	public int generator()
 	{
 		int num = 1 + (int)(Math.random() * 3);
 		return num;
 	} // end method generator
 	
-	// display winner or loser depending upon user's choice
+	// -----------------------------------display winner or loser depending upon user's choice
+	// This is the game engine method.
+	// Determine winner and display results
+	// Please think about another Method , like CheckWinner, that only checks winner.
+	// Display would only DISPLAY winner
+	
 	public void Display()
 	{
 		if(super.getPlay() == randoms)
